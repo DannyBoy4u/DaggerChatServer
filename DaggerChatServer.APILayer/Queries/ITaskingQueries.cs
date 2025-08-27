defb737868af938ac88d3ff638bb5327044b6add
DannyBoy4u <65839992+DannyBoy4u@users.noreply.github.com>
2025-08-27 14:59:56 -0500

    Update ITaskingQueries.cs

--- DaggerChatServer.APILayer/Queries/ITaskingQueries.cs
+++ DaggerChatServer.APILayer/Queries/ITaskingQueries.cs
@@ -1,5 +1,7 @@
-﻿using DatabaseLayer;
+using APILayer.DTOs;
+using DatabaseLayer;
 using DatabaseLayer.Entities;
+using Mapster;
 using Microsoft.EntityFrameworkCore;
 
 namespace APILayer.Queries
@@ -7,6 +9,8 @@ namespace APILayer.Queries
     public interface ITaskingQueries
     {
         Task<List<WorkTask>> GetAllTasks();
+        Task InsertWorkTask(GetWorkItemFullDTO workTask);
+        Task<bool> TaskExistenceCheckByUrl(string url);
     }
 
     public class TaskingQueries : ITaskingQueries
@@ -18,5 +22,13 @@ namespace APILayer.Queries
         }
 
         public async Task<List<WorkTask>> GetAllTasks() => await _databaseContext.WorkTasks.ToListAsync();
+
+        public async Task InsertWorkTask(GetWorkItemFullDTO workTask)
+        {
+            await _databaseContext.WorkTasks.AddAsync(workTask.Adapt<WorkTask>());
+            await _databaseContext.SaveChangesAsync();
+        }
+
+        public async Task<bool> TaskExistenceCheckByUrl(string url) => await _databaseContext.WorkTasks.AnyAsync(task => task.UrlLink.ToLower() == url.ToLower());
     }
 }
